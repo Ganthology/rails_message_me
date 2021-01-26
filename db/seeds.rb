905895f96ceb8708ccc6071d01b5851ac9be7b29
Ganthology <gbk1386@gmail.com>
2021-01-26 15:42:26 +0800

    Add message model, The chatroom now display the message from user

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# User.create(username: 'demo1', password: 'demo')
+# User.create(username: 'demo2', password: 'demo')
+# User.create(username: 'demo3', password: 'demo')
+# User.create(username: 'demo4', password: 'demo')
+# User.create(username: 'demo5', password: 'demo')
+
+Message.create(body: 'This is message 1', user_id: 1)
+Message.create(body: 'This is message 2', user_id: 2)
+Message.create(body: 'This is message 3', user_id: 3)
+Message.create(body: 'This is message 4', user_id: 4)
+Message.create(body: 'This is message 5', user_id: 5)
